f74ef6bb1f6f7911eb8b245468ea3ed5e877c6e7
Ruben Berenguel Montoro <ruben@mostlymaths.net>
2025-01-26 21:56:30 +0100

    Started tinkering

--- roids/roids.js
+++ roids/roids.js
@@ -1,3 +1,5 @@
+import { set, get } from "../libs/3rdparty/idb-keyval.js";
+
 import {
   Application,
   Graphics,
@@ -5,8 +7,57 @@ import {
   Matrix,
 } from "../libs/3rdparty/pixi.mjs";
 
+import {
+  bindGamepadHandlers,
+  bindKeyHandlers,
+  handleControls,
+  getDeviceInput,
+} from "../libs/controlHandling.js";
+
+bindGamepadHandlers();
+bindKeyHandlers();
+
+let keyMap = await get("keyMap");
+
+if (keyMap === undefined) {
+  keyMap = {
+    ArrowUp: "moveUp",
+    ArrowDown: "moveDown",
+    ArrowLeft: "moveLeft",
+    ArrowRight: "moveRight",
+  };
+}
+
+let buttonMap = await get("buttonMap");
+
+if (buttonMap === undefined) {
+  buttonMap = {
+    b15: "moveRight",
+    b14: "moveLeft",
+    b13: "moveDown",
+    b12: "moveUp",
+  };
+}
+
+const gameActions = {
+  moveUp: () => {},
+  moveDown: () => {
+    const [sx, sy] = rotate(-5, 0, ship.r);
+    for (let i = 0; i < 4; i++) {
+      addFlames(ship.x + sx, ship.y + sy, -2, 0, 10, 0);
+    }
+    ship.vx += 0.1 * Math.cos(ship.r);
+    ship.vy += 0.1 * Math.sin(ship.r);
+  },
+  moveRight: () => {
+    ship.r += 0.1;
+  },
+  moveLeft: () => {
+    ship.r -= 0.1;
+  },
+};
+
 let flameList = [];
-let flameObjects = [];
 
 const rotate = (x1, x2, ang) => {
   const cos = Math.cos(ang);
@@ -23,8 +74,8 @@ const addFlames = (x, y, vx, vy, e, d) => {
   let flame = {
     x: x,
     y: y,
-    vx: vx + accel*Math.cos(d)*rf,
-    vy: vy + accel*Math.sin(d)*rf,
+    vx: vx + accel * Math.cos(d) * rf,
+    vy: vy + accel * Math.sin(d) * rf,
     e: e,
   };
   const [nvx, nvy] = rotate(flame.vx, flame.vy, r);
@@ -39,25 +90,31 @@ await app.init({ width: 1040, height: 1040 });
 document.body.appendChild(app.canvas);
 
 // Polygon (Ship)
-const ship = new Graphics();
+const ship = {
+  x: 200,
+  y: 200,
+  vx: 0,
+  vy: 0,
+  r: 0,
+};
+ship.pres = new Graphics();
 
 const path = [-70, 50, 70, 0, -70, -50, -30, 0, -70, 50];
-ship.poly(path); // Draw the polygon at its initial position (relative to itself)
+ship.pres.poly(path); // Draw the polygon at its initial position (relative to itself)
 
-ship.scale.set(0.2);
+ship.pres.scale.set(0.2);
 //ship.fill(0x3500fa);
-ship.stroke({ color: 0xffffff, width: 10 });
-app.stage.addChild(ship);
-ship.x = 300;
-ship.y = 200;
-
-for(let i=0; i<10; i++){
-    addFlames(290, 200, -2, 0, 10, 0);
-}
-
+ship.pres.stroke({ color: 0xffffff, width: 10 });
+app.stage.addChild(ship.pres);
 
 app.ticker.add((delta) => {
+  handleControls(gameActions, keyMap, buttonMap);
   //ship.x += delta.deltaTime;
+  ship.x += ship.vx * delta.deltaTime;
+  ship.y += ship.vy * delta.deltaTime;
+  ship.pres.x = ship.x;
+  ship.pres.y = ship.y;
+  ship.pres.angle = ship.r * (180 / Math.PI);
   for (const fl of flameList) {
     if (fl.drawn) {
       continue;
@@ -85,8 +142,5 @@ app.ticker.add((delta) => {
     const hexColor = (red << 16) | (green << 8) | blue;
     fl.pres.tint = hexColor;
   }
-  flameList = flameList.filter(fl => fl.e > 0.1)
-  for(let i=0; i<4; i++){
-    addFlames(290, 200, -2, 0, 10, 0);
-    }
+  flameList = flameList.filter((fl) => fl.e > 0.1);
 });
